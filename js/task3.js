345c27fd70306b7f7a15aeb94c878d6517de5bfc
Lilia Lomaka <llomaka80@gmail.com>
2022-01-20 19:53:26 +0200

    Task 3 completed

--- js/task3.js
+++ js/task3.js
@@ -3,6 +3,29 @@
 /*
  * Напиши класс User который создаёт объект со свойствами login и email. Объяви приватные свойства #login и #email, доступ к которым сделай через геттер и сеттер login и email.
  */
+class User {
+    #login;
+    #email;
+
+    constructor({ login, email }) {
+        this.#login = login;
+        this.#email = email;
+    }
+
+    get login() {
+        return this.#login;
+    }
+    get email() {
+        return this.#email;
+    }
+    set login(newLogin) {
+        this.#login = newLogin;
+    }
+    set email(newEmail) {
+        this.#email = newEmail;
+    }
+}
+
 
 const mango = new User({
     login: 'Mango',
